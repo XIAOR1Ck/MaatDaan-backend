a4038523bbd92a0b8186743819d52062cf268120
Suresh Acharya <sureshach.off@gmail.com>
2026-08-10 07:18:09 +0545

    Add middleware to webauthn routes

--- src/routes/webauthn.routes.ts
+++ src/routes/webauthn.routes.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import * as controller from '../controllers/webauthn.controller';
+import { protect } from "../middleware/auth";
 
 const webauthnRouter = Router();
-webauthnRouter.post('/register/options', controller.registerOptions);
-webauthnRouter.post('/register/verify', controller.registerVerify);
+webauthnRouter.post('/register/options', protect, controller.registerOptions);
+webauthnRouter.post('/register/verify',protect, controller.registerVerify);
 
-webauthnRouter.post('/login/options', controller.loginOptions);
-webauthnRouter.post('/login/verify', controller.loginVerify);
+webauthnRouter.post('/login/options', protect, controller.loginOptions);
+webauthnRouter.post('/login/verify', protect, controller.loginVerify);
 
 
 export default webauthnRouter;
